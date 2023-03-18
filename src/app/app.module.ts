4d44e554d24be48e9eab79f05f68e888d0857ac2
SharonInfante <sharonshabel@gmail.com>
2023-03-18 13:11:42 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    ButtonsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
